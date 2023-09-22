1c8fc1639ad908e2f3027a2a32a940923a6efe5e
Aymen13h <aymen.kharrat13@gmail.com>
2023-09-22 04:04:04 +0100

    Promises and Async

---
+++ Promises.js
@@ -0,0 +1,22 @@
+function flippingACoin() {
+    return new Promise((resolve, reject) => {
+      const val = Math.round(Math.random() * 1); 
+  
+      val ? resolve('Heads!!') : reject('Tails!!');
+    });
+  }
+  
+  async function result() {
+    try {
+      const result = await flippingACoin();
+      console.log(result);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  result(); 
+  result(); 
+  result(); 
+  result(); 
+  result(); 
